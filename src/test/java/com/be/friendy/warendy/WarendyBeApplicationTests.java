b54e20c7e262140eb0514ccc68ef9bd6e6d9a1e6
wookieOH <ehddnrdh@gmail.com>
2023-07-16 13:17:35 +0900

    spring boot init

---
+++ src/test/java/com/be/friendy/warendy/WarendyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.friendy.warendy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarendyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
