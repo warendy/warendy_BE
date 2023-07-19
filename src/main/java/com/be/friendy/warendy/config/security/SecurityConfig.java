19fe86c130b76f762372c26ef07a17aaea6080bf
tkddbs1008 <finibin08@gmail.com>
2023-07-19 14:21:38 +0900

    feat: #6-signup/signin

---
+++ src/main/java/com/be/friendy/warendy/config/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.be.friendy.warendy.config.security;
+
+
+import com.be.friendy.warendy.config.JwtAuthenticationFilter;
+import com.be.friendy.warendy.exception.MemberAuthFailureHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
+        http.csrf().disable();
+
+        http.authorizeHttpRequests(authorize -> {
+            try {
+                authorize
+                            .requestMatchers("/login/**", "/signup/**", "/oauth2/**", "/**")
+                            .permitAll() // 해당 경로는 인증 없이 접근 가능
+                            .requestMatchers("/member/**") // 해당 경로는 인증이 필요
+                            .hasRole("MEMBER") // ROLE 이 MEMBER 가 포함된 경우에만 인증 가능
+                        .and()
+                            .addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/be/friendy/warendy/exception/MemberAuthFailureHandler.java
@@ -0,0 +1,24 @@
+package com.be.friendy.warendy.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class MemberAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws ServletException, IOException {
+        // 실패 메세지를 담기 위한 세션 선언
+        HttpSession session = request.getSession();
+        // 세션에 실패 메세지 담기
+        session.setAttribute("loginErrorMessage", exception.getMessage());
+        // 로그인 실패시 이동할 페이지
+        setDefaultFailureUrl("/login/error");
+        // 로그인 실패시 이동할 페이지로 이동
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
