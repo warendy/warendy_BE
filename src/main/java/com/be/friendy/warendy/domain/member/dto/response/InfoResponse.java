37d2dac0c53beac894afca4f7662f97499d5b32c
tkddbs1008 <finibin08@gmail.com>
2023-07-25 15:42:49 +0900

    create record class for InfoResponse

--- src/main/java/com/be/friendy/warendy/domain/member/dto/response/InfoResponse.java
+++ src/main/java/com/be/friendy/warendy/domain/member/dto/response/InfoResponse.java
@@ -1,32 +1,22 @@
 package com.be.friendy.warendy.domain.member.dto.response;
 
-import com.be.friendy.warendy.domain.member.dto.request.SignUpRequest;
 import com.be.friendy.warendy.domain.member.entity.Member;
 import com.be.friendy.warendy.domain.member.entity.constant.Role;
 import lombok.*;
 
-@Setter
-@Getter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class InfoResponse {
-
-    private String email;
-    private String nickname;
-    private String password;
-    private String avatar;
-
-    private Role role;
-    private String oauthType;
-
-    private int body;
-    private int dry;
-    private int tannin;
-    private int acidity;
-
-
+public record InfoResponse (
+        String email,
+        String nickname,
+        String password,
+        String avatar,
+        Role role,
+        String oauthType,
+        int body,
+        int dry,
+        int tannin,
+        int acidity
+) {
 
     public static InfoResponse fromEntity(Member member) {
         return InfoResponse.builder()
