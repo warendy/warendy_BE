a9f656f90b6eb762c00541cf83c54c26e609ec74
chanee <hanssang6301@gmail.com>
2023-08-05 22:00:02 +0900

    fix: wine pairing 바뀐 타입을 수정 및 pairing 오타 수정, wine pairing 타입 : list에서 -> String으로 오타 수정 : paring -> pairing

--- src/test/java/com/be/friendy/warendy/domain/wine/controller/WineControllerTest.java
+++ src/test/java/com/be/friendy/warendy/domain/wine/controller/WineControllerTest.java
@@ -1,87 +1,84 @@
-//package com.be.friendy.warendy.domain.wine.controller;
-//
-//import com.be.friendy.warendy.config.AppConfig;
-//import com.be.friendy.warendy.config.jwt.TokenProvider;
-//import com.be.friendy.warendy.config.jwt.filter.JwtAuthenticationFilter;
-//import com.be.friendy.warendy.config.security.SecurityConfig;
-//import com.be.friendy.warendy.domain.review.entity.Review;
-//import com.be.friendy.warendy.domain.wine.dto.response.WineDetailSearchResponse;
-//import com.be.friendy.warendy.domain.wine.service.WineService;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.FilterType;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.mockito.ArgumentMatchers.anyLong;
-//import static org.mockito.BDDMockito.given;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@WebMvcTest(value = WineController.class,
-//        excludeFilters = {
-//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-//                        classes = SecurityConfig.class),
-//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-//                        classes = AppConfig.class),
-//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-//                        classes = JwtAuthenticationFilter.class),
-//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-//                        classes = TokenProvider.class)
-//        }
-//)
-//class WineControllerTest {
-//
-//    @MockBean
-//    private WineService wineService;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Test
-//    @WithMockUser(roles = "MEMBER")
-//    @DisplayName("success search wine detail! - wine")
-//    void successSearchWineDetail() throws Exception {
-//        //given
-//        List<String> food = new ArrayList<>();
-//        food.add("apple");
-//        food.add("banana");
-//        List<Review> reviewList = new ArrayList<>();
-//        given(wineService.searchWineDetail(anyLong()))
-//                .willReturn(WineDetailSearchResponse.builder()
-//                        .wineName("wineName")
-//                        .vintage(2014)
-//                        .price("W 25,000")
-//                        .picture("url")
-//                        .body(1)
-//                        .dry(1)
-//                        .tannin(1)
-//                        .acidity(1)
-//                        .alcohol(1.1)
-//                        .grapes("grapes")
-//                        .pairing(food)
-//                        .region("region")
-//                        .type("wineStyle")
-//                        .winery("winery")
-//                        .rating(1.1f)
-//                        .reviewList(reviewList)
-//                        .build());
-//        //when
-//        //then
-//        mockMvc.perform(get("/wines/1/detail")).andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.paring[1]")
-//                        .value("banana"))
-//        ;
-//    }
-//
-//}
+package com.be.friendy.warendy.domain.wine.controller;
+
+import com.be.friendy.warendy.config.AppConfig;
+import com.be.friendy.warendy.config.jwt.TokenProvider;
+import com.be.friendy.warendy.config.jwt.filter.JwtAuthenticationFilter;
+import com.be.friendy.warendy.config.security.SecurityConfig;
+import com.be.friendy.warendy.domain.review.entity.Review;
+import com.be.friendy.warendy.domain.wine.dto.response.WineDetailSearchResponse;
+import com.be.friendy.warendy.domain.wine.service.WineService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = WineController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = SecurityConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = AppConfig.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = JwtAuthenticationFilter.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                        classes = TokenProvider.class)
+        }
+)
+class WineControllerTest {
+
+    @MockBean
+    private WineService wineService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(roles = "MEMBER")
+    @DisplayName("success search wine detail! - wine")
+    void successSearchWineDetail() throws Exception {
+        //given
+        List<Review> reviewList = new ArrayList<>();
+        given(wineService.searchWineDetail(anyLong()))
+                .willReturn(WineDetailSearchResponse.builder()
+                        .wineName("wineName")
+                        .vintage(2014)
+                        .price("W 25,000")
+                        .picture("url")
+                        .body(1)
+                        .dry(1)
+                        .tannin(1)
+                        .acidity(1)
+                        .alcohol(1.1)
+                        .grapes("grapes")
+                        .pairing("food")
+                        .region("region")
+                        .type("wineStyle")
+                        .winery("winery")
+                        .rating(1.1f)
+                        .reviewList(reviewList)
+                        .build());
+        //when
+        //then
+        mockMvc.perform(get("/wines/1/detail")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.pairing")
+                        .value("food"))
+        ;
+    }
+
+}
